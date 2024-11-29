f7be6cbae4e763a414284ffe301970493ef00861
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-29 15:58:22 +0100

    get all users route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,36 @@
-export async function GET() {
-  const data = await fetch("https://api.vercel.app/blog");
-  const posts = await data.json();
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/app/_utils/db/db";
 
-  return Response.json({ message: "success", data: posts });
+import { authMiddleware } from "../authMiddleware";
+
+export async function GET(request: NextRequest) {
+  const authResponse = await authMiddleware(request);
+  if (!authResponse.ok) {
+    return authResponse; // If unauthorized, return the middleware response
+  }
+
+  try {
+    const users = await prisma.user.findMany();
+    const sanitizedUsers = users.map(
+      ({ passwordHash, verificationToken, ...rest }) => rest
+    );
+
+    if (!sanitizedUsers) {
+      return NextResponse.json(
+        { error: "Korisnici nisu pronađeni" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { results: sanitizedUsers.length, data: sanitizedUsers },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Greška prilikom preuzimanja korisnika" },
+      { status: 500 }
+    );
+  }
 }
