c1efd13befa5342f7ada63546bb051c826029968
Aleksandar <aleksandar@e-vlasotince.info>
2024-11-29 17:09:45 +0100

    reprograming authMiddlwaere

--- app/api/authMiddleware.ts
+++ app/api/authMiddleware.ts
@@ -1,17 +1,29 @@
 import { NextResponse, NextRequest } from "next/server";
 import { verifyJWT } from "../_utils/auth";
-//import { jwtVerify } from "jose";
+import { JWTPayload } from "jose";
 
-//const secretKey = process.env.JWT_SECRET!;
+type AuthResponseSuccess = {
+  userId: JWTPayload["userId"]; // Adjust `JWTPayload` to the actual type returned by `verifyJWT`.
+};
 
-export async function authMiddleware(request: NextRequest) {
+type AuthResponseError = {
+  error: string;
+  status: number;
+};
+
+export async function authMiddleware(
+  request: NextRequest
+): Promise<
+  NextResponse<AuthResponseError> | NextResponse<AuthResponseSuccess>
+> {
   const cookieHeader = request.headers.get("cookie");
 
   if (!cookieHeader) {
-    return NextResponse.json(
-      { error: "Pristip nije autorizovan." },
-      { status: 401 }
-    );
+    return NextResponse.json({
+      error: "Pristup nije autorizovan.",
+      status: 401,
+      userId: null,
+    });
   }
 
   const cookies = Object.fromEntries(
@@ -20,20 +32,23 @@ export async function authMiddleware(request: NextRequest) {
   const token = cookies.authToken;
 
   if (!token) {
-    return NextResponse.json(
-      { error: "Pristip nije autorizovan." },
-      { status: 401 }
-    );
+    return NextResponse.json({
+      error: "Pristup nije autorizovan.",
+      status: 401,
+      userId: null,
+    });
   }
 
   try {
-    await verifyJWT(token);
-    return NextResponse.next(); // Allow the request
+    const { payload } = await verifyJWT(token);
+
+    return NextResponse.json({ userId: payload.userId });
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
-    return NextResponse.json(
-      { error: "Pristip nije autorizovan." },
-      { status: 401 }
-    );
+    return NextResponse.json({
+      error: "Pristup nije autorizovan.",
+      status: 401,
+      userId: null,
+    });
   }
 }
