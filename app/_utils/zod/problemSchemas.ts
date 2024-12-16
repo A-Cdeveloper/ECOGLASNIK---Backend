28d68bfbc03cc10259947bda695b6006c02f8240
Aleksandar <aleksandar@e-vlasotince.info>
2024-12-16 01:27:33 +0100

    set problem image not required

--- app/_utils/zod/problemSchemas.ts
+++ app/_utils/zod/problemSchemas.ts
@@ -19,7 +19,7 @@ const problemBaseSchema = z.object({
     .max(300, "Opis problema mora biti maksimalno 300 karaktera")
     .transform((val) => val.trim()),
   cat_id: z.number().int(),
-  image: z.string().min(1, "Fotografija problema je obavezna").optional(),
+  image: z.string().optional(),
   pinata_id: z.string().optional(),
 });
 
