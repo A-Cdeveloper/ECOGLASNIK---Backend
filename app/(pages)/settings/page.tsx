b25e94aad4469554a70277af71d33bea7674eb6c
Aleksandar <aleksandar@e-vlasotince.info>
2025-01-06 02:44:18 +0100

    update react and install laflet again

--- app/(pages)/settings/page.tsx
+++ app/(pages)/settings/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Headline from "../../_components/ui/Headline";
-// import EditSettings from "./_components/EditSettings";
+import EditSettings from "./_components/EditSettings";
 
 const PageSettings = () => {
   return (
     <>
       <Headline level={1}>Podešavanja aplikacije</Headline>
-      {/* <EditSettings /> */}
+      <EditSettings />
     </>
   );
 };
