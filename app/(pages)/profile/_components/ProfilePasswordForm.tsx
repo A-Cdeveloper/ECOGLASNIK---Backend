ec2e6b26aa1b55120e1a5cd01620455e77b025e2
Aleksandar <aleksandar@e-vlasotince.info>
2025-01-18 02:49:32 +0100

    fix unused files - workign versions  lint errors

--- app/(pages)/profile/_components/ProfilePasswordForm.tsx
+++ app/(pages)/profile/_components/ProfilePasswordForm.tsx
@@ -2,14 +2,14 @@
 
 import { SubmitButton } from "@/app/_components/ui/Buttons/SubmitButton";
 import Input from "@/app/_components/ui/Form/Input";
-import { useActionState } from "react";
+// import { useActionState } from "react";
 
 import { UserRestrictedType } from "@/app/_utils/db/prismaTypes";
 
-import ToggleSwitch from "@/app/_components/ui/Form/ToggleSwitch";
-import ErrorsForm from "../../../_components/ui/Form/ErrorsForm";
-import { addNewUserAction, updateUserAction } from "../_actions";
-import Select from "@/app/_components/ui/Form/Select";
+// import ToggleSwitch from "@/app/_components/ui/Form/ToggleSwitch";
+// import ErrorsForm from "../../../_components/ui/Form/ErrorsForm";
+// import { addNewUserAction, updateUserAction } from "../_actions";
+// import Select from "@/app/_components/ui/Form/Select";
 
 const ProfilePasswordForm = ({ user }: { user?: UserRestrictedType }) => {
   ////////////
