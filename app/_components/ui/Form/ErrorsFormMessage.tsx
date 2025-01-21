d60b25b6cbdd6d05efe80a2050cbd0d5b7507594
Aleksandar <aleksandar@e-vlasotince.info>
2025-01-21 16:45:43 +0100

    set tiem out on error messages

--- app/_components/ui/Form/ErrorsFormMessage.tsx
+++ app/_components/ui/Form/ErrorsFormMessage.tsx
@@ -1,6 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
+
 const ErrorsFormMessage = ({ errors }: { errors: string[] }) => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (errors.length > 0) {
+      setVisible(false);
+      const timer = setTimeout(() => {
+        setVisible(true);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [errors]);
+
+  if (visible) {
+    return null;
+  }
+
   return (
-    <div className=" text-danger-100 w-full text-[13px] my-1">
+    <div className=" text-danger-100 w-full text-[13px] my-2">
       {errors.map((error) => (
         <p key={error}>{error}</p>
       ))}
