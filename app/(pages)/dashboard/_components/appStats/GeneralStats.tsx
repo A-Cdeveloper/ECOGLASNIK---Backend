3d1a3e6366436ef41feb36c32c4bcf67883b6256
Aleksandar <aleksandar@e-vlasotince.info>
2025-02-03 18:10:09 +0100

    fix key issue on dashboard

--- app/(pages)/dashboard/_components/appStats/GeneralStats.tsx
+++ app/(pages)/dashboard/_components/appStats/GeneralStats.tsx
@@ -14,24 +14,28 @@ const GeneralStats = async () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-[1fr_1fr_1fr_1fr] my-8 gap-2 w-full 2xl:w-[60%] ">
       <StatBox
+        key={1}
         total={categories}
         label="Kategorije"
         icon={<HiOutlineSquare3Stack3D />}
         link="categories"
       />
       <StatBox
+        key={2}
         total={organisations}
         label="Službe"
         icon={<HiOutlineWrenchScrewdriver />}
         link="organisations"
       />
       <StatBox
+        key={3}
         total={problems}
         label="Problemi"
         icon={<HiOutlineBellAlert />}
         link="problems"
       />
       <StatBox
+        key={4}
         total={users}
         label="Korisnici"
         icon={<HiOutlineUsers />}
