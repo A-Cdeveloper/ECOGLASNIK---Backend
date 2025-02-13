260b3c2e0c4a1a232f2e2dca45fcb5382e8343e9
Aleksandar <aleksandar@e-vlasotince.info>
2025-02-13 15:08:21 +0100

    dinamic db

--- app/_utils/db/db.ts
+++ app/_utils/db/db.ts
@@ -1,15 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({});
+const prismaClientSingleton = (databaseUrl: string) => {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: databaseUrl, // Dynamically set DATABASE_URL
+      },
+    },
+  });
 };
 
 declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+  prismaGlobal?: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const databaseUrl = process.env.DATABASE_URL ?? ""; // Get DATABASE_URL dynamically
+
+if (!databaseUrl) {
+  throw new Error("❌ DATABASE_URL is not set.");
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton(databaseUrl);
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prismaGlobal = prisma;
+}
