f5e325dc61172508d5231c264f214b44e4190bfb
Aleksandar <aleksandar@e-vlasotince.info>
2025-02-13 16:16:59 +0100

    revert to basic setting for db (one domain = one db)

--- app/_utils/db/db.ts
+++ app/_utils/db/db.ts
@@ -1,36 +1,15 @@
-// src/prismaClient.ts
-import { getDatabaseUrl } from "@/app/config";
 import { PrismaClient } from "@prisma/client";
 
-// Function to create Prisma Client based on a dynamic database URL
-const prismaClientSingleton = (databaseUrl: string) => {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: databaseUrl, // Dynamically set DATABASE_URL
-      },
-    },
-  });
+const prismaClientSingleton = () => {
+  return new PrismaClient({});
 };
 
-// Declare the global object for development
 declare const globalThis: {
-  prismaGlobal?: ReturnType<typeof prismaClientSingleton>;
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-// Get the appropriate DATABASE_URL based on domain or environment
-const databaseUrl = getDatabaseUrl();
-
-if (!databaseUrl) {
-  throw new Error("❌ DATABASE_URL is not set.");
-}
-
-// Use cached Prisma client in development or create a new one in production
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton(databaseUrl);
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
 
-// Cache the Prisma client instance globally in development
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prismaGlobal = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
--- app/config/index.ts
+++ app/config/index.ts
@@ -1,12 +1,5 @@
 import { bannedWords } from "../config/bannedWords";
 import { urls } from "./urls";
-// Convert words to regex patterns with variations
-export const bannedRegex = new RegExp(
-  bannedWords
-    .map((word) => word.split("").join("+") + "+") // Handle repeated letters (e.g., "govnoo", "kuraaac")
-    .join("|"), // Join into a single regex
-  "gi" // Global and case-insensitive
-);
 
 const getFrontendBaseUrl = () => {
   const hostname = process.env.VERCEL_URL || "localhost";
@@ -24,22 +17,13 @@ const getFrontendBaseUrl = () => {
   return "https://www.demo.ecoglasnik.org"; // Fallback
 };
 
-export const getDatabaseUrl = () => {
-  const hostname = process.env.VERCEL_URL || "localhost"; // Get Vercel URL or fallback to localhost
-
-  if (hostname.includes("localhost")) {
-    return process.env.DATABASE_URL_LOCALHOST; // Local environment URL
-  }
-
-  for (const url of urls) {
-    if (hostname.includes(url)) {
-      return process.env[`DATABASE_URL_${url.toUpperCase()}`]; // Dynamic environment variable based on domain
-    }
-  }
-
-  return process.env.DATABASE_URL_DEFAULT; // Fallback URL for other cases
-};
-
 export const MAX_UPLOAD_FILE_SIZE = 10 * 1024 * 1024;
 export const MAX_PAGE_SIZE = 10;
+// Convert words to regex patterns with variations
+export const bannedRegex = new RegExp(
+  bannedWords
+    .map((word) => word.split("").join("+") + "+") // Handle repeated letters (e.g., "govnoo", "kuraaac")
+    .join("|"), // Join into a single regex
+  "gi" // Global and case-insensitive
+);
 export const BASE_URL = getFrontendBaseUrl();
