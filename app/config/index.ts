6f93052b503bca6ec4c12849bf258a07242819e7
Aleksandar <aleksandar@e-vlasotince.info>
2025-02-20 01:21:27 +0100

    change config

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,18 +1,4 @@
 import { bannedWords } from "../config/bannedWords";
-import { urls } from "./urls";
-
-const getFrontendBaseUrl = () => {
-  const hostname = process.env.VERCEL_URL || "localhost";
-
-  if (hostname.includes("localhost")) {
-    return "http://localhost:5173"; // Local Frontend
-  }
-
-  const matchedUrl = urls.find((url) => hostname.includes(url));
-  return matchedUrl
-    ? `https://www.${matchedUrl}.ecoglasnik.org`
-    : "https://www.demo.ecoglasnik.org"; // Fallback URL
-};
 
 export const MAX_UPLOAD_FILE_SIZE = 10 * 1024 * 1024;
 export const MAX_PAGE_SIZE = 10;
@@ -23,4 +9,4 @@ export const bannedRegex = new RegExp(
     .join("|"), // Join into a single regex
   "gi" // Global and case-insensitive
 );
-export const BASE_URL = getFrontendBaseUrl();
+export const BASE_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
--- app/config/urls.ts
+++
@@ -1,1 +0,0 @@
-export const urls = ["demo", "blank", "vlasotince", "leskovac"];
